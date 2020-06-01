0ba68ca34dab37ed54aa8cf7a75d6e9b0a5aa143
alexkimeu0 <kim3ual3x@gmail.com>
2020-06-01 21:48:45 +0300

    added user search functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,55 @@
 document.addEventListener("DOMContentLoaded", loadUsers);
 
-let usersDiv = document.getElementById("users");
-document.getElementById("searchForm").addEventListener("submit", searchUser);
-
 const homeURL = "https://api.github.com/users";
+let usersDiv = document.getElementById("users");
+let title = document.getElementById("page-title");
+let form = document.getElementById("searchForm");
+
+form.onsubmit = function (e) {
+  let searchTxt = document.getElementById("searchTxt").value;
+
+  e.preventDefault();
+
+  let xhr = new XMLHttpRequest();
+
+  xhr.open("GET", `https://api.github.com/search/users?q=${searchTxt}`, true);
+
+  xhr.onload = function () {
+    if (this.status == 200) {
+      let res = JSON.parse(this.responseText);
+
+      let users = res.items;
+
+      let output = "";
+
+      for (var u in users) {
+        output += `
+            <div class="col-md-3">
+                <div class="card card-body">
+                <img src="${users[u].avatar_url}" class="img-fluid">
+                <h6><strong>Username:</strong>&nbsp;${users[u].login}</h6>
+               <a href="${users[u].html_url}"
+                 target="_blank" class="btn btn-secondary">
+                 <i class="fas fa-user-circle"></i>
+                 Profile
+                 </a>
+                </div>
+            </div>
+          `;
+      }
+
+      if (users.length > 0 && users !== undefined) {
+        title.innerHTML = `Search Results for: ${searchTxt}`;
+      } else {
+        title.innerHTML = `No Results for: ${searchTxt}`;
+      }
+
+      usersDiv.innerHTML = output;
+    }
+  };
+
+  xhr.send();
+};
 
 function loadUsers() {
   let xhr = new XMLHttpRequest();
@@ -27,50 +73,16 @@ function loadUsers() {
                <a href="${users[u].html_url}"
                  target="_blank" class="btn btn-secondary">
                  <i class="fas fa-user-circle"></i>
-                 View profile
+                 Profile
                  </a>
                 </div>
             </div>
           `;
       }
+      title.innerHTML = "First 100 Github Users:";
       usersDiv.innerHTML = output;
     }
   };
 
   xhr.send();
-}
-
-function searchUser(e) {
-  let searchTxt = document.getElementById("searchTxt").value;
-
-  e.preventDefault();
-
-  let xhr = new XMLHttpRequest();
-
-  xhr.open("GET", "https://api.github.com/search/users?q=" + searchTxt, true);
-
-  xhr.onload = function () {
-    if (this.status == 200) {
-      let users = JSON.parse(this.responseText);
-      console.log(users);
-
-      let output = "";
-
-      for (var u in users) {
-        output += `
-            <div class="col-md-3">
-                <div class="card card-body">
-                <img src="${users[u].avatar_url}" class="img-fluid">
-                <h6>Username:&nbsp;${users[u].login}</h6>
-                <h6>Profile:&nbsp;<a href="${users[u].html_url}" target="_blank">${users[u].login}</a></h6>
-                </div>
-            </div>
-          `;
-      }
-      usersDiv.innerHTML = output;
-    } else {
-      console.log("Error");
-    }
-    xhr.send();
-  };
 }
